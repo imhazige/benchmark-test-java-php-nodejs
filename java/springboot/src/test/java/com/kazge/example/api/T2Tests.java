b547c2fb9a91c29141af3bc137b2dec0d90d1971
Ka <imhazige@gmail.com>
2018-02-02 18:13:05 +0800

    add auth fail test case, but it did not work as the testresttemplate will throw error when encounter a 401 code.

--- java/springboot/src/test/java/com/kazge/example/api/T2Tests.java
+++ java/springboot/src/test/java/com/kazge/example/api/T2Tests.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import javax.validation.constraints.AssertTrue;
 import java.util.Map;
 import java.util.UUID;
 
@@ -39,6 +38,23 @@ public class T2Tests extends BaseTTests {
 
         u.setId(respUSer.getId());
         return doGetToken(u);
+    }
+
+    @Test
+    public void testGetTokenError() {
+        //use t1 to create a user to login
+        User u = new User();
+        u.setName(UUID.randomUUID().toString());
+        u.setPassword("123456");
+        ResponseEntity<String> response = post("/t1/users", null, u);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        User respUSer = JacksonUtils.parse(response.getBody(), User.class);
+
+        u.setId(respUSer.getId());
+        //can not test error when response 401
+//        u.setPassword(u.getPassword() + "_no");
+        response = post(getBaseUrl() + "/token", null, u);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
