5d3de2d7f37a49bcf4a5cd9c9e56d018b57d133f
Ka <imhazige@gmail.com>
2018-02-08 15:32:37 +0800

    nodejs win java

--- nodejs/express/common/log.js
+++ nodejs/express/common/log.js
@@ -1,7 +1,7 @@
 const log4js = require('log4js');
 log4js.configure({
     appenders: { cheese: { type: 'file', filename: 'express-benmark.log' }, 'out': { type: 'stdout' } },
-    categories: { default: { appenders: ['cheese','out'], level: 'debug' } }
+    categories: { default: { appenders: ['cheese','out'], level: 'error' } }
 });
 
 //use default
